46f92a6dd752d9fd68b1aea522e27fb3d24a9d4d
Michael Olufuwa <olufuwa.michael@gmail.com>
2018-01-22 19:29:20 +0000

    Implemented Sass setup

---
+++ css/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    bust = require('gulp-buster');
+
+gulp.task('sass', function() {
+    gulp.src('style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    	.pipe(gulp.dest('.'))
+    	.pipe(rename({suffix: '.min'}))
+    	.pipe(minifycss())
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('watch',function() {
+    gulp.watch(['sass/**/*.scss'],['sass']);
+});
